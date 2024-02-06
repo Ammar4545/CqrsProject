f123a82f2dafd020f3196b77be8ef5883899195d
Ammar Mahmoud <ammar.m777.2@gmail.com>
2024-02-06 09:15:37 +0200

    clean the arch

--- CqrsProject.Api/Program.cs
+++ CqrsProject.Api/Program.cs
@@ -9,10 +9,19 @@ namespace CqrsProject.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "CQRS Api v1");
+                });
+            }
 
             app.UseHttpsRedirection();
 
